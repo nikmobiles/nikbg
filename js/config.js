7851f2216dd95e2c5030a6c8da7657c24a697949
nikmobiles <83655891+nikmobiles@users.noreply.github.com>
2021-12-18 17:27:09 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,16 +1,18 @@
 cCode = 'BG'; // if '' => reads cCode from folder name or query string, if 'IP' => reads cCode from IP address, or set manually
-
-datingApp = 'sms:187077?;&body=MEGA Таня ви изпрати снимка и покана отговори с ДА и я приеми';
-regRoute = 'sms:187077?;&body=MEGA Таня ви изпрати снимка и покана отговори с ДА и я приеми'; // set custom reg route, default is /register-user-terminal
+userType = 'soi';
+contentType = 'familySafe'; // adult || familySafe
+apiKey = ' AIzaSyACS-8RzKUsCEJOkkYRUcwb3P2Oq9P1eyU';
+datingApp = 'https://poluchime.com/';
+regRoute = ''; // set custom reg route, default is /register-user-terminal
 emailOnly = 1; // 1 to activate email only mode, default null
 googleBtnText = ''; // set custom text or default text if empty
-redirection = 'sms:187077?;&body=MEGA Таня ви изпрати снимка и покана отговори с ДА и я приеми'; // choose redirection to reg1 to finish registration
+redirection = true; // choose redirection to reg1 to finish registration
 flow = false; // set auto picking dating app
 cookieBar = false; // show/hide cookie bar
 storeData = true; // choose to store data in local storage or not
 giftCredits = null; // in this moment it will be null on frontend, maybe in the future we will give user some value to credit after register
-accId = 'AW-350898554'; // familySafe only
-accHash = 'H_RiCI-rw8ICEPqSqacB'; // familySafe only
+accId = 'AW-3508985541'; // familySafe only
+accHash = 'H_RiCI-rw8ICEPqSqacB1'; // familySafe only
 domain = 'poluchime.com'; // set custom domain name in tos or leave empty for deafault
 documentTitle = 'random'; // random for random title, set custom title or leave empty for default title
 customTextUrl = ''; // define custom text file path
